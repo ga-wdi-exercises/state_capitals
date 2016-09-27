178f7b0b4c96db3d2aeb362d2b7376c9352b5287
Mike Flores <michaelprflores@gmail.com>
2016-09-27 16:56:26 -0400

    This is as far as I got before 5 PM

--- capitals-development.rb
+++ capitals-development.rb
@@ -13,42 +13,34 @@ states =[
     capital: "Dover"
 }]
 
-# shuffles and alters the states variable, this works!
-states.shuffle! 
+
+
 
 # Greets the user, this works!
-# puts "Hey there! What's your name?"
-# user_name = gets 
-# puts "Welcome #{user_name} Here's the first state!"
+puts "Hey there! What's your name?"
+user_name = gets.chomp 
+puts "Welcome #{user_name}! Let the game begin!"
 
-# Loops through states
+# shuffles and alters the states variable, this works!
+states.shuffle! 
+# Loops through states & initializes new keys in the hashes that store the number of times
+# a user gets a capital `correct` and the number of times the answer is `wrong`
 states.each do |state|
-    score = 0
     puts "What is the capital of " + state[:name] + "?"
     user_answer = gets.chomp
-    if user_answer == state[:capital]
-        puts "You got it right!"
-        state.merge!(score:"right")
-        score += 1
-    else
-        puts "You got it wrong!"
-        state.merge!(score:"wrong")
-        score -= 1
-    end
+        correct = 0
+        total = states.length
+        if user_answer == state[:capital]
+            puts "You got it right!"
+            state.merge!(score:"right")
+            # correct.to_i +=
+        else
+            puts "You got it wrong!"
+            state.merge!(score:"wrong")
+        end
 end
 
-# Initializes new keys in the hashes that store the number of times
-# a user gets a capital `correct` and the number of times the answer is `wrong`.
-
-# puts "What is the capital of " + states[0][:name] + "?"
-# user_answer = gets
-# if user_answer === states[0][:capital]
-#     puts "You got it right!"
-#     # states[0].push[:score => "right"]
-# else
-#     puts "You got it wrong!" 
-#     # states[0].push[:score => "wrong"]
-# end
+puts "You got #{correct} out of #{total}!"
 
 binding.pry
 puts "Finished!"
