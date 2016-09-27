619c34c7d8abd48033c2785c43456e25ebc79897
Latoya Watson <latoyamwatson@gmail.com>
2016-09-27 16:55:28 -0400

    not finished but geting there. time to submit

--- capitals.rb
+++ capitals.rb
@@ -1,35 +1,177 @@
-# require 'pry'
-
 # an array of state hashes
 states =[
 {
+    name: "Alabama",
+    capital: "Montgomery"
+}, {
+    name: "Alaska",
+    capital: "Juneau"
+}, {
+    name: "Arizona",
+    capital: "Phoenix"
+}, {
+    name: "Arkansas",
+    capital: "Little Rock"
+}, {
+    name: "California",
+    capital: "Sacramento"
+}, {
+    name: "Colorado",
+    capital: "Denver"
+}, {
+    name: "Connecticut",
+    capital: "Hartford"
+}, {
+    name: "Delaware",
+    capital: "Dover"
+}, {
+    name: "Florida",
+    capital: "Tallahassee"
+}, {
+    name: "Georgia",
+    capital: "Atlanta"
+}, {
+    name: "Hawaii",
+    capital: "Honolulu"
+}, {
+    name: "Idaho",
+    capital: "Boise"
+}, {
+    name: "Illinois",
+    capital: "Springfield"
+}, {
+    name: "Indiana",
+    capital: "Indianapolis"
+}, {
+    name: "Iowa",
+    capital: "Des Moines"
+}, {
+    name: "Kansas",
+    capital: "Topeka"
+}, {
+    name: "Kentucky",
+    capital: "Frankfort"
+}, {
+    name: "Louisiana",
+    capital: "Baton Rouge"
+}, {
+    name: "Maine",
+    capital: "Augusta"
+}, {
+    name: "Maryland",
+    capital: "Annapolis"
+}, {
+    name: "Massachusetts",
+    capital: "Boston"
+}, {
+    name: "Michigan",
+    capital: "Lansing"
+}, {
+    name: "Minnesota",
+    capital: "St. Paul"
+}, {
+    name: "Mississippi",
+    capital: "Jackson"
+}, {
+    name: "Missouri",
+    capital: "Jefferson City"
+}, {
+    name: "Montana",
+    capital: "Helena"
+}, {
+    name: "Nebraska",
+    capital: "Lincoln"
+}, {
+    name: "Nevada",
+    capital: "Carson City"
+}, {
+    name: "New Hampshire",
+    capital: "Concord"
+}, {
+    name: "New Jersey",
+    capital: "Trenton"
+}, {
+    name: "New Mexico",
+    capital: "Santa Fe"
+}, {
     name: "New York",
     capital: "Albany"
 }, {
     name: "North Carolina",
     capital: "Raleigh"
 }, {
+    name: "North Dakota",
+    capital: "Bismarck"
+}, {
     name: "Ohio",
     capital: "Columbus"
+}, {
+    name: "Oklahoma",
+    capital: "Oklahoma City"
+}, {
+    name: "Oregon",
+    capital: "Salem"
+}, {
+    name: "Pennsylvania",
+    capital: "Harrisburg"
+}, {
+    name: "Rhode Island",
+    capital: "Providence"
+}, {
+    name: "South Carolina",
+    capital: "Columbia"
+}, {
+    name: "South Dakota",
+    capital: "Pierre"
+}, {
+    name: "Tennessee",
+    capital: "Nashville"
+}, {
+    name: "Texas",
+    capital: "Austin"
+}, {
+    name: "Utah",
+    capital: "Salt Lake City"
+}, {
+    name: "Vermont",
+    capital: "Montpelier"
+}, {
+    name: "Virginia",
+    capital: "Richmond"
+}, {
+    name: "Washington",
+    capital: "Olympia"
+}, {
+    name: "West Virginia",
+    capital: "Charleston"
+}, {
+    name: "Wisconsin",
+    capital: "Madison"
+}, {
+    name: "Wyoming",
+    capital: "Cheyenne"
 }].shuffle
 
-index = 0
+puts "Let's play - What's that capital!?"
 
+index = 0
 while true do
   while true do
-    wrong = 0
-    right = 0
-    puts "What's capital of #{states[index][:name]} ?"
+states.each {
+  |state| state[:correct] = 0
+  state[:wrong] = 0
+}
+    puts "What's the capital of #{states[index][:name]} ?"
     user_input = gets.chomp
     if user_input != states[index][:capital]
-        wrong += 1
+        states[index][:wrong] =+ 1
         puts "Try again"
     end
     break if (user_input == states[index][:capital])
   end
-  index += 1
-  right += 1
-  puts "You guessed wrong #{wrong} times and got it correct: #{right} times!"
+  index =+ 1
+  states[index][:right] =+ 1
+  puts "You guessed wrong #{states[index][:wrong]} times and got it correct: #{states[index][:right]} times!"
 end
 
 
