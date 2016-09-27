ce7aab038c415f478c22ba8ba780a0657d8a7d55
Latoya Watson <latoyamwatson@gmail.com>
2016-09-27 16:35:13 -0400

    added guesses right and wrong tally v2

--- capitals.rb
+++ capitals.rb
@@ -1,4 +1,4 @@
-require 'pry'
+# require 'pry'
 
 # an array of state hashes
 states =[
@@ -11,26 +11,25 @@ states =[
 }, {
     name: "Ohio",
     capital: "Columbus"
-}]
+}].shuffle
 
 index = 0
-guesses_wrong = 0
-guesses_right = 0
 
 while true do
   while true do
-    puts "What is the capital of #{states[index][:name]} ?"
+    wrong = 0
+    right = 0
+    puts "What's capital of #{states[index][:name]} ?"
     user_input = gets.chomp
     if user_input != states[index][:capital]
-        guesses_wrong += 1
+        wrong += 1
         puts "Try again"
-        puts "You have guessed wrong #{guesses_wrong} times"
     end
     break if (user_input == states[index][:capital])
   end
   index += 1
-  guesses_right += 1
-  puts "You tried #{guesses_wrong} times and got it correct: #{guesses_right} times!"
+  right += 1
+  puts "You guessed wrong #{wrong} times and got it correct: #{right} times!"
 end
 
 
@@ -39,5 +38,5 @@ end
 
 # states.shuffle!  to keep the states shuffled always
 
-binding.pry
-puts "stop pry bug"
+# binding.pry
+# puts "stop pry bug"
