f59d0c159d71e7afa92f64e6d8b2c54aced52f7b
Mike Flores <michaelprflores@gmail.com>
2016-09-27 15:56:12 -0400

    feature: loops through the hashes within states

--- capitals-development.rb
+++ capitals-development.rb
@@ -22,13 +22,39 @@ states =[
     capital: "Honolulu"
 }]
 
-# shuffles and alters the states variable 
+# shuffles and alters the states variable, this works!
 states.shuffle! 
 
-# Greets the user
-puts "Hey there! What's your name?"
-user_name = gets 
-puts "Welcome " + user_name + "! Here's the first state!"
+# Greets the user, this works!
+# puts "Hey there! What's your name?"
+# user_name = gets 
+# puts "Welcome " + user_name + "! Here's the first state!"
+
+# Loops through states
+states.each do |state|
+    puts "What is the capital of " + state[:name] + "?"
+    user_answer = gets.chomp
+    if user_answer == state[:capital]
+        puts "You got it right!"
+        # states[0].push[:score => "right"]
+        else
+        puts "You got it wrong!" 
+        # states[0].push[:score => "wrong"]
+    end
+end
+
+# Initializes new keys in the hashes that store the number of times
+# a user gets a capital `correct` and the number of times the answer is `wrong`.
+
+# puts "What is the capital of " + states[0][:name] + "?"
+# user_answer = gets
+# if user_answer === states[0][:capital]
+#     puts "You got it right!"
+#     # states[0].push[:score => "right"]
+# else
+#     puts "You got it wrong!" 
+#     # states[0].push[:score => "wrong"]
+# end
 
 binding.pry
 puts "Finished!"
