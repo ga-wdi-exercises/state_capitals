f06ebf9406fed30ef4480cd943b70571ea1021ff
Donovan Allen <dallen.928@gmail.com>
2016-10-25 11:31:33 -0400

    initial commit

--- capitals.rb
+++ capitals.rb
@@ -1,4 +1,4 @@
-# an array of state hashes
+# # an array of state hashes
 states =[
 {
     name: "Alabama",
@@ -151,3 +151,70 @@ states =[
     name: "Wyoming",
     capital: "Cheyenne"
 }]
+# states = [
+#   {
+#       name: "Alabama",
+#       capital: "Montgomery"
+#   }, {
+#       name: "Hawaii",
+#       capital: "Honolulu"
+#   }, {
+#       name: "Minnesota",
+#       capital: "St. Paul"
+#   }
+# ]
+
+correct_states = [
+
+]
+
+incorrect_states = [
+
+]
+
+score = {
+  correct: 0,
+  incorrect: 0
+}
+
+#Welcome
+puts "Welcome to the State Capital Game. I say a state and you give the capital. Type 'play' to start the game!"
+
+def play_game(states, score)
+  #if answer is correct, display msg, increment correct, show answers (remove state from array?)
+  states = states.shuffle
+  states.each do |state|
+    #Prompt user for capital of state
+    puts "#{state[:name]}?" #"(Enter 'h' for a hint)"
+    user_answer = gets.chomp
+    if user_answer == state[:capital]
+      score[:correct] += 1
+      puts "****Correct!****"
+      puts score
+    # elsif user_answer == "h"
+    #   puts "#{state[:name]}? Hint: #{state[:name[0..2]]}"
+    else
+      score[:incorrect] += 1
+      puts "Wrong! The answer is #{state[:capital]}."
+      puts score
+
+    end
+  end
+end
+
+#starts game
+play = gets.chomp
+if play == "play"
+  puts "Great, let's play!"
+  play_game(states, score)
+end
+puts "Your score is #{score[:correct]}/50"
+puts "Would you like to play again? y/n"
+play_again = gets.chomp
+if play_again == "y"
+  play_game(states, score)
+elsif play_again == "n"
+  puts "Thanks for playing!"
+end
+
+#
