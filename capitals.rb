c38cdef8055f445e79cdc6c2ba3ebc4a2cb42bc1
Thinh Nguyen <nguyendthinh@outlook.com>
2016-10-25 12:15:19 -0400

    finished lab

--- capitals.rb
+++ capitals.rb
@@ -1,4 +1,9 @@
 # an array of state hashes
+answers = {
+  correct: 0,
+  wrong: 0
+}
+
 states =[
 {
     name: "Alabama",
@@ -18,136 +23,179 @@ states =[
 }, {
     name: "Colorado",
     capital: "Denver"
-}, {
-    name: "Connecticut",
-    capital: "Hartford"
-}, {
-    name: "Delaware",
-    capital: "Dover"
-}, {
-    name: "Florida",
-    capital: "Tallahassee"
-}, {
-    name: "Georgia",
-    capital: "Atlanta"
-}, {
-    name: "Hawaii",
-    capital: "Honolulu"
-}, {
-    name: "Idaho",
-    capital: "Boise"
-}, {
-    name: "Illinois",
-    capital: "Springfield"
-}, {
-    name: "Indiana",
-    capital: "Indianapolis"
-}, {
-    name: "Iowa",
-    capital: "Des Moines"
-}, {
-    name: "Kansas",
-    capital: "Topeka"
-}, {
-    name: "Kentucky",
-    capital: "Frankfort"
-}, {
-    name: "Louisiana",
-    capital: "Baton Rouge"
-}, {
-    name: "Maine",
-    capital: "Augusta"
-}, {
-    name: "Maryland",
-    capital: "Annapolis"
-}, {
-    name: "Massachusetts",
-    capital: "Boston"
-}, {
-    name: "Michigan",
-    capital: "Lansing"
-}, {
-    name: "Minnesota",
-    capital: "St. Paul"
-}, {
-    name: "Mississippi",
-    capital: "Jackson"
-}, {
-    name: "Missouri",
-    capital: "Jefferson City"
-}, {
-    name: "Montana",
-    capital: "Helena"
-}, {
-    name: "Nebraska",
-    capital: "Lincoln"
-}, {
-    name: "Nevada",
-    capital: "Carson City"
-}, {
-    name: "New Hampshire",
-    capital: "Concord"
-}, {
-    name: "New Jersey",
-    capital: "Trenton"
-}, {
-    name: "New Mexico",
-    capital: "Santa Fe"
-}, {
-    name: "New York",
-    capital: "Albany"
-}, {
-    name: "North Carolina",
-    capital: "Raleigh"
-}, {
-    name: "North Dakota",
-    capital: "Bismarck"
-}, {
-    name: "Ohio",
-    capital: "Columbus"
-}, {
-    name: "Oklahoma",
-    capital: "Oklahoma City"
-}, {
-    name: "Oregon",
-    capital: "Salem"
-}, {
-    name: "Pennsylvania",
-    capital: "Harrisburg"
-}, {
-    name: "Rhode Island",
-    capital: "Providence"
-}, {
-    name: "South Carolina",
-    capital: "Columbia"
-}, {
-    name: "South Dakota",
-    capital: "Pierre"
-}, {
-    name: "Tennessee",
-    capital: "Nashville"
-}, {
-    name: "Texas",
-    capital: "Austin"
-}, {
-    name: "Utah",
-    capital: "Salt Lake City"
-}, {
-    name: "Vermont",
-    capital: "Montpelier"
-}, {
-    name: "Virginia",
-    capital: "Richmond"
-}, {
-    name: "Washington",
-    capital: "Olympia"
-}, {
-    name: "West Virginia",
-    capital: "Charleston"
-}, {
-    name: "Wisconsin",
-    capital: "Madison"
-}, {
-    name: "Wyoming",
-    capital: "Cheyenne"
-}]
+}
+# {
+#     name: "Connecticut",
+#     capital: "Hartford"
+# }, {
+#     name: "Delaware",
+#     capital: "Dover"
+# }, {
+#     name: "Florida",
+#     capital: "Tallahassee"
+# }, {
+#     name: "Georgia",
+#     capital: "Atlanta"
+# }, {
+#     name: "Hawaii",
+#     capital: "Honolulu"
+# }, {
+#     name: "Idaho",
+#     capital: "Boise"
+# }, {
+#     name: "Illinois",
+#     capital: "Springfield"
+# }, {
+#     name: "Indiana",
+#     capital: "Indianapolis"
+# }, {
+#     name: "Iowa",
+#     capital: "Des Moines"
+# }, {
+#     name: "Kansas",
+#     capital: "Topeka"
+# }, {
+#     name: "Kentucky",
+#     capital: "Frankfort"
+# }, {
+#     name: "Louisiana",
+#     capital: "Baton Rouge"
+# }, {
+#     name: "Maine",
+#     capital: "Augusta"
+# }, {
+#     name: "Maryland",
+#     capital: "Annapolis"
+# }, {
+#     name: "Massachusetts",
+#     capital: "Boston"
+# }, {
+#     name: "Michigan",
+#     capital: "Lansing"
+# }, {
+#     name: "Minnesota",
+#     capital: "St. Paul"
+# }, {
+#     name: "Mississippi",
+#     capital: "Jackson"
+# }, {
+#     name: "Missouri",
+#     capital: "Jefferson City"
+# }, {
+#     name: "Montana",
+#     capital: "Helena"
+# }, {
+#     name: "Nebraska",
+#     capital: "Lincoln"
+# }, {
+#     name: "Nevada",
+#     capital: "Carson City"
+# }, {
+#     name: "New Hampshire",
+#     capital: "Concord"
+# }, {
+#     name: "New Jersey",
+#     capital: "Trenton"
+# }, {
+#     name: "New Mexico",
+#     capital: "Santa Fe"
+# }, {
+#     name: "New York",
+#     capital: "Albany"
+# }, {
+#     name: "North Carolina",
+#     capital: "Raleigh"
+# }, {
+#     name: "North Dakota",
+#     capital: "Bismarck"
+# }, {
+#     name: "Ohio",
+#     capital: "Columbus"
+# }, {
+#     name: "Oklahoma",
+#     capital: "Oklahoma City"
+# }, {
+#     name: "Oregon",
+#     capital: "Salem"
+# }, {
+#     name: "Pennsylvania",
+#     capital: "Harrisburg"
+# }, {
+#     name: "Rhode Island",
+#     capital: "Providence"
+# }, {
+#     name: "South Carolina",
+#     capital: "Columbia"
+# }, {
+#     name: "South Dakota",
+#     capital: "Pierre"
+# }, {
+#     name: "Tennessee",
+#     capital: "Nashville"
+# }, {
+#     name: "Texas",
+#     capital: "Austin"
+# }, {
+#     name: "Utah",
+#     capital: "Salt Lake City"
+# }, {
+#     name: "Vermont",
+#     capital: "Montpelier"
+# }, {
+#     name: "Virginia",
+#     capital: "Richmond"
+# }, {
+#     name: "Washington",
+#     capital: "Olympia"
+# }, {
+#     name: "West Virginia",
+#     capital: "Charleston"
+# }, {
+#     name: "Wisconsin",
+#     capital: "Madison"
+# }, {
+#     name: "Wyoming",
+#     capital: "Cheyenne"
+# }
+]
+
+states.map{|state| state[:correct] = 0}
+
+states.shuffle!
+
+puts "Hello player, your goal is to guess as many capitals of each states as possible, type your name to begin the game."
+name = gets.chomp
+puts "Alright #{name}, let's get started."
+
+index = 0
+while index < states.length
+  puts "What is the capital of #{states[index][:name]}? (Type 'hint' if you need help)"
+  capital = gets.chomp
+  if capital == "hint"
+    puts "The first three letter of this capital is #{states[index][:capital][0,3]}. Please type your answer now."
+    capital = gets.chomp
+  end
+    if capital == states[index][:capital]
+    answers[:correct] += 1
+    states[index][:correct] += 1
+    puts "Correct."
+    puts "Score: #{answers[:correct]} out of #{index + 1}."
+    puts "You guessed the capital of this state right #{states[index][:correct]} times!"
+    else
+    puts "Incorrect."
+    puts "Score: #{answers[:correct]} out of #{index + 1}."
+    puts "You guessed the capital of this state right #{states[index][:correct]} times!"
+    end
+    index += 1
+  if index == states.length
+    puts "Thank you for playing the game, would you like to play again? Type yes or no"
+    continue = gets.chomp
+    if continue == "yes"
+      index = 0
+      answers[:correct] = 0
+      answers[:wrong] = 0
+      states.shuffle!
+      states.sort_by{|name, capital, correct| correct}
+    end
+  end
+end
