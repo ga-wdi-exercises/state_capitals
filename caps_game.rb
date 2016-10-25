847bf2b53dd5ba2d8796ee7af1f6070daee0e279
robert <robert.k25@gmail.com>
2016-10-25 12:48:02 -0400

    formatting and minor fixes

--- caps_game.rb
+++ caps_game.rb
@@ -10,23 +10,33 @@ def add_scoring (states)
   }
 end
 
+def get_scores(states)
+  correct_list = states.collect {|state| state[:correct]}
+  incorrect_list = states.collect {|state| state[:incorrect]}
+  correct = correct_list.reduce(:+)
+  incorrect = incorrect_list.reduce(:+)
+  return correct, incorrect
+end
+
 def play_round (states)
-  states.shuffle!
-  states.each { |state|
+  correct, incorrect = get_scores(states)
+  states.shuffle.each { |state|
     puts "What is the capital #{state[:name]}?"
+    puts "Correct: #{correct}  Incorrect: #{incorrect}"
     answer = gets.chomp
     if answer.capitalize == state[:capital]
       puts "Correct!"
       state[:correct] += 1
+      correct += 1
     else
       puts "Incorrect"
       state[:incorrect] += 1
+      incorrect +=1
     end
   }
 end
 
 add_scoring(states)
-puts states
 
 puts "Welcome to my struggle game about states and their capitals"
 
@@ -34,7 +44,5 @@ loop {
   play_round states
   puts "Again? ((y)es to play another round)"
   continue = gets.chomp
-  if continue != 'y'  && continue != 'yes'
-    break
-  end
+  break if continue != 'y'  && continue != 'yes'
 }
