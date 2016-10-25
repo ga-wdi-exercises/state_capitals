47a2b2f538a91de29acd06ae5da2a13cb15e3ce3
Drew Swinburne <drewswinburne@gmail.com>
2016-10-25 10:55:29 -0400

    lol this is garbage code

--- capitals.rb
+++ capitals.rb
@@ -1,5 +1,5 @@
 # # an array of state hashes
-states =[
+$states =[
 # {
 #     name: "Alabama",
 #     capital: "Montgomery"
@@ -153,44 +153,56 @@ states =[
     capital: "Cheyenne"
 }]
 
+$answered = 0
+$correct_answers = 0
 
-answered = 0
-correct_answers = 0
-
-puts "Welcome to state capital guessing!"
-states.shuffle.each do |state|
-puts "what is the capital of #{state[:name]}?"
-answer = gets.chomp
-answered = answered + 1
-state[:correct] = 0
-state[:wrong] = 0
-if answer == "#{state[:capital]}"
-  state[:correct] = (state[:correct] + 1)
-  correct_answers = correct_answers + 1
-  puts "correct!"
-else puts "nope!"
-  state[:wrong] = (state[:wrong] + 1)
-end
-puts "you've answered #{correct_answers} correct out of #{answered} total states"
+def playagain
+  $states.shuffle.each do |state|
+  puts "what is the capital of #{state[:name]}?"
+  answer = gets.chomp
+  $answered = $answered + 1
+  if answer == "#{state[:capital]}"
+    state[:correct] = (state[:correct] + 1)
+    $correct_answers = $correct_answers + 1
+    puts "correct! you've answered #{state[:name]} correctly #{state[:correct]} times"
+  else puts "nope!"
+    state[:wrong] = (state[:wrong] + 1)
+  end
+  puts "you've answered #{$correct_answers} correct out of #{$answered} total"
 end
 puts "Would you like to play again? (y/n)"
 again = gets.chomp
   if again == "n"
   puts "Okay have a good day!"
 elsif again == "y"
-    states.shuffle.each do |state|
-    puts "what is the capital of #{state[:name]}?"
-    answer = gets.chomp
-    answered = answered + 1
-    if answer == "#{state[:capital]}"
-      state[:correct] = (state[:correct] + 1)
-      correct_answers = correct_answers + 1
-      puts "correct! you've answered #{state[:name]} correctly #{state[:correct]} times"
-    else puts "nope!"
-      state[:wrong] = (state[:wrong] + 1)
-    end
-    puts "you've answered #{correct_answers} correct out of #{answered} total"
-  end
+  playagain
+end
+end
 
 
+
+
+
+puts "Welcome to state capital guessing!"
+$states.shuffle.each do |state|
+puts "what is the capital of #{state[:name]}?"
+answer = gets.chomp
+$answered = $answered + 1
+state[:correct] = 0
+state[:wrong] = 0
+if answer == "#{state[:capital]}"
+  state[:correct] = (state[:correct] + 1)
+  $correct_answers = $correct_answers + 1
+  puts "correct!"
+else puts "nope!"
+  state[:wrong] = (state[:wrong] + 1)
+end
+puts "you've answered #{$correct_answers} correct out of #{$answered} total states"
+end
+puts "Would you like to play again? (y/n)"
+again = gets.chomp
+  if again == "n"
+  puts "Okay have a good day!"
+elsif again == "y"
+    playagain()
 end
