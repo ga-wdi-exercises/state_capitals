0fb36adcd0abf16a87e9115acd337dc1f3637f9d
Sam Catherman <Sam.Catherman@gmail.com>
2016-10-25 14:08:44 -0400

    moves test code to main game file. adds code comments

--- capitals.rb
+++ capitals.rb
@@ -151,23 +151,56 @@ states =[
     name: "Wyoming",
     capital: "Cheyenne"
 }]
-
-states = states.shuffle
-
-puts "welcome to the game"
-
-states.each do |state|
-  state[:correct] = 0
-  state[:incorrect] = 0
+#initializes count object, sets variables to zero
+count = {
+  correct: 0,
+  incorrect: 0
+}
+#welcomes user
+puts "WELCOME."
+#method to run game using states array, count as arguments
+def run_game (states, count)
+  #shuffles states
+  states = states.shuffle
+  #for each state
+  states.each do |state|
+    #picks state, asks user for capital
+    puts "Guess capital of: " + state[:name]
+    #stores user input to response variable
+    response = gets.chomp
+    #if the response is correct:
+    if response == state[:capital]
+      #alerts user
+      puts "Correct!"
+      #increases correct count by 1
+      count[:correct] += 1
+    #otherwise:
+    else
+      #alerts wrong answer
+      puts "Wrong!"
+      #increases incorrect count by 1
+      count[:incorrect] += 1
+    #ends answer checker
+    end
+    #displays updated count
+    puts "Incorrect: " + count[:incorrect].to_s
+    puts "Correct: " + count[:correct].to_s
+  end
 end
-
-puts "Correctly guess the capital of: " + states[0][:name]
-response = gets.chomp
-
-if response == states[0][:capital]
-  puts "Correct!"
-  # states[:correct] += 1
-else response != states[0][:capital]
-  puts "Incorrect! The correct answer is: " + state[:capital]
-#   state[:incorrect] += 1
+#loads game upon start
+run_game(states, count)
+#displays win ratio
+puts "You guessed " + count[:correct].to_s + " out of 50"
+#asks user if they would like to play again
+puts "Would you like to play again? (Y/N)"
+#stores user input to answer variable
+answer = gets.chomp
+#if they want to play again
+if answer.upcase == "Y"
+  #run the game again
+  run_game(states, count)
+#otherwise
+else
+#say goodbye
+  puts("goodbye!")
 end
