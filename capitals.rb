6210483373bc1ef2f583ff4cc4e50795f9cea952
Drew Swinburne <drewswinburne@gmail.com>
2016-10-25 11:14:42 -0400

    on restart, user only asked states they got wrong

--- capitals.rb
+++ capitals.rb
@@ -1,147 +1,147 @@
 # # an array of state hashes
 $states =[
-# {
-#     name: "Alabama",
-#     capital: "Montgomery"
-# }, {
-#     name: "Alaska",
-#     capital: "Juneau"
-# }, {
-#     name: "Arizona",
-#     capital: "Phoenix"
-# }, {
-#     name: "Arkansas",
-#     capital: "Little Rock"
-# }, {
-#     name: "California",
-#     capital: "Sacramento"
-# }, {
-#     name: "Colorado",
-#     capital: "Denver"
-# }, {
-#     name: "Connecticut",
-#     capital: "Hartford"
-# }, {
-#     name: "Delaware",
-#     capital: "Dover"
-# }, {
-#     name: "Florida",
-#     capital: "Tallahassee"
-# }, {
-#     name: "Georgia",
-#     capital: "Atlanta"
-# }, {
-#     name: "Hawaii",
-#     capital: "Honolulu"
-# }, {
-#     name: "Idaho",
-#     capital: "Boise"
-# }, {
-#     name: "Illinois",
-#     capital: "Springfield"
-# }, {
-#     name: "Indiana",
-#     capital: "Indianapolis"
-# }, {
-#     name: "Iowa",
-#     capital: "Des Moines"
-# }, {
-#     name: "Kansas",
-#     capital: "Topeka"
-# }, {
-#     name: "Kentucky",
-#     capital: "Frankfort"
-# }, {
-#     name: "Louisiana",
-#     capital: "Baton Rouge"
-# }, {
-#     name: "Maine",
-#     capital: "Augusta"
-# }, {
-#     name: "Maryland",
-#     capital: "Annapolis"
-# }, {
-#     name: "Massachusetts",
-#     capital: "Boston"
-# }, {
-#     name: "Michigan",
-#     capital: "Lansing"
-# }, {
-#     name: "Minnesota",
-#     capital: "St. Paul"
-# }, {
-#     name: "Mississippi",
-#     capital: "Jackson"
-# }, {
-#     name: "Missouri",
-#     capital: "Jefferson City"
-# }, {
-#     name: "Montana",
-#     capital: "Helena"
-# }, {
-#     name: "Nebraska",
-#     capital: "Lincoln"
-# }, {
-#     name: "Nevada",
-#     capital: "Carson City"
-# }, {
-#     name: "New Hampshire",
-#     capital: "Concord"
-# }, {
-#     name: "New Jersey",
-#     capital: "Trenton"
-# }, {
-#     name: "New Mexico",
-#     capital: "Santa Fe"
-# }, {
-#     name: "New York",
-#     capital: "Albany"
-# }, {
-#     name: "North Carolina",
-#     capital: "Raleigh"
-# }, {
-#     name: "North Dakota",
-#     capital: "Bismarck"
-# }, {
-#     name: "Ohio",
-#     capital: "Columbus"
-# }, {
-#     name: "Oklahoma",
-#     capital: "Oklahoma City"
-# }, {
-#     name: "Oregon",
-#     capital: "Salem"
-# }, {
-#     name: "Pennsylvania",
-#     capital: "Harrisburg"
-# }, {
-#     name: "Rhode Island",
-#     capital: "Providence"
-# }, {
-#     name: "South Carolina",
-#     capital: "Columbia"
-# }, {
-#     name: "South Dakota",
-#     capital: "Pierre"
-# }, {
-#     name: "Tennessee",
-#     capital: "Nashville"
-# }, {
-#     name: "Texas",
-#     capital: "Austin"
-# }, {
-#     name: "Utah",
-#     capital: "Salt Lake City"
-# }, {
-#     name: "Vermont",
-#     capital: "Montpelier"
-# }, {
-#     name: "Virginia",
-#     capital: "Richmond"
-# }, {
-#     name: "Washington",
-#     capital: "Olympia"
-# },
+{
+    name: "Alabama",
+    capital: "Montgomery"
+}, {
+    name: "Alaska",
+    capital: "Juneau"
+}, {
+    name: "Arizona",
+    capital: "Phoenix"
+}, {
+    name: "Arkansas",
+    capital: "Little Rock"
+}, {
+    name: "California",
+    capital: "Sacramento"
+}, {
+    name: "Colorado",
+    capital: "Denver"
+}, {
+    name: "Connecticut",
+    capital: "Hartford"
+}, {
+    name: "Delaware",
+    capital: "Dover"
+}, {
+    name: "Florida",
+    capital: "Tallahassee"
+}, {
+    name: "Georgia",
+    capital: "Atlanta"
+}, {
+    name: "Hawaii",
+    capital: "Honolulu"
+}, {
+    name: "Idaho",
+    capital: "Boise"
+}, {
+    name: "Illinois",
+    capital: "Springfield"
+}, {
+    name: "Indiana",
+    capital: "Indianapolis"
+}, {
+    name: "Iowa",
+    capital: "Des Moines"
+}, {
+    name: "Kansas",
+    capital: "Topeka"
+}, {
+    name: "Kentucky",
+    capital: "Frankfort"
+}, {
+    name: "Louisiana",
+    capital: "Baton Rouge"
+}, {
+    name: "Maine",
+    capital: "Augusta"
+}, {
+    name: "Maryland",
+    capital: "Annapolis"
+}, {
+    name: "Massachusetts",
+    capital: "Boston"
+}, {
+    name: "Michigan",
+    capital: "Lansing"
+}, {
+    name: "Minnesota",
+    capital: "St. Paul"
+}, {
+    name: "Mississippi",
+    capital: "Jackson"
+}, {
+    name: "Missouri",
+    capital: "Jefferson City"
+}, {
+    name: "Montana",
+    capital: "Helena"
+}, {
+    name: "Nebraska",
+    capital: "Lincoln"
+}, {
+    name: "Nevada",
+    capital: "Carson City"
+}, {
+    name: "New Hampshire",
+    capital: "Concord"
+}, {
+    name: "New Jersey",
+    capital: "Trenton"
+}, {
+    name: "New Mexico",
+    capital: "Santa Fe"
+}, {
+    name: "New York",
+    capital: "Albany"
+}, {
+    name: "North Carolina",
+    capital: "Raleigh"
+}, {
+    name: "North Dakota",
+    capital: "Bismarck"
+}, {
+    name: "Ohio",
+    capital: "Columbus"
+}, {
+    name: "Oklahoma",
+    capital: "Oklahoma City"
+}, {
+    name: "Oregon",
+    capital: "Salem"
+}, {
+    name: "Pennsylvania",
+    capital: "Harrisburg"
+}, {
+    name: "Rhode Island",
+    capital: "Providence"
+}, {
+    name: "South Carolina",
+    capital: "Columbia"
+}, {
+    name: "South Dakota",
+    capital: "Pierre"
+}, {
+    name: "Tennessee",
+    capital: "Nashville"
+}, {
+    name: "Texas",
+    capital: "Austin"
+}, {
+    name: "Utah",
+    capital: "Salt Lake City"
+}, {
+    name: "Vermont",
+    capital: "Montpelier"
+}, {
+    name: "Virginia",
+    capital: "Richmond"
+}, {
+    name: "Washington",
+    capital: "Olympia"
+},
 {
     name: "West Virginia",
     capital: "Charleston"
@@ -158,6 +158,9 @@ $correct_answers = 0
 
 def playagain
   $states.shuffle.each do |state|
+    if state[:correct] > 0
+        next
+    end
   puts "what is the capital of #{state[:name]}?"
   answer = gets.chomp
   $answered = $answered + 1
@@ -180,9 +183,6 @@ end
 end
 
 
-
-
-
 puts "Welcome to state capital guessing!"
 $states.shuffle.each do |state|
 puts "what is the capital of #{state[:name]}?"
@@ -197,7 +197,7 @@ if answer == "#{state[:capital]}"
 else puts "nope!"
   state[:wrong] = (state[:wrong] + 1)
 end
-puts "you've answered #{$correct_answers} correct out of #{$answered} total states"
+puts "you've answered #{$correct_answers} correct out of #{$answered} total"
 end
 puts "Would you like to play again? (y/n)"
 again = gets.chomp
