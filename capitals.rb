844cad159fdb817bcaf12cedf8c6cd398fa210d2
Diana B. Gobar <dianabgobar@gmail.com>
2016-10-25 11:25:36 -0400

    Basics finished

--- capitals.rb
+++ capitals.rb
@@ -9,167 +9,178 @@ states =[
 }, {
     name: "Arizona",
     capital: "Phoenix"
-#}, {
-#     name: "Arkansas",
-#     capital: "Little Rock"
-# }, {
-#     name: "California",
-#     capital: "Sacramento"
-# }, {
-#     name: "Colorado",
-#     capital: "Denver"
-# }, {
-#     name: "Connecticut",
-#     capital: "Hartford"
-# }, {
-#     name: "Delaware",
-#     capital: "Dover"
-# }, {
-#     name: "Florida",
-#     capital: "Tallahassee"
-# }, {
-#     name: "Georgia",
-#     capital: "Atlanta"
-# }, {
-#     name: "Hawaii",
-#     capital: "Honolulu"
-# }, {
-#     name: "Idaho",
-#     capital: "Boise"
-# }, {
-#     name: "Illinois",
-#     capital: "Springfield"
-# }, {
-#     name: "Indiana",
-#     capital: "Indianapolis"
-# }, {
-#     name: "Iowa",
-#     capital: "Des Moines"
-# }, {
-#     name: "Kansas",
-#     capital: "Topeka"
-# }, {
-#     name: "Kentucky",
-#     capital: "Frankfort"
-# }, {
-#     name: "Louisiana",
-#     capital: "Baton Rouge"
-# }, {
-#     name: "Maine",
-#     capital: "Augusta"
-# }, {
-#     name: "Maryland",
-#     capital: "Annapolis"
-# }, {
-#     name: "Massachusetts",
-#     capital: "Boston"
-# }, {
-#     name: "Michigan",
-#     capital: "Lansing"
-# }, {
-#     name: "Minnesota",
-#     capital: "St. Paul"
-# }, {
-#     name: "Mississippi",
-#     capital: "Jackson"
-# }, {
-#     name: "Missouri",
-#     capital: "Jefferson City"
-# }, {
-#     name: "Montana",
-#     capital: "Helena"
-# }, {
-#     name: "Nebraska",
-#     capital: "Lincoln"
-# }, {
-#     name: "Nevada",
-#     capital: "Carson City"
-# }, {
-#     name: "New Hampshire",
-#     capital: "Concord"
-# }, {
-#     name: "New Jersey",
-#     capital: "Trenton"
-# }, {
-#     name: "New Mexico",
-#     capital: "Santa Fe"
-# }, {
-#     name: "New York",
-#     capital: "Albany"
-# }, {
-#     name: "North Carolina",
-#     capital: "Raleigh"
-# }, {
-#     name: "North Dakota",
-#     capital: "Bismarck"
-# }, {
-#     name: "Ohio",
-#     capital: "Columbus"
-# }, {
-#     name: "Oklahoma",
-#     capital: "Oklahoma City"
-# }, {
-#     name: "Oregon",
-#     capital: "Salem"
-# }, {
-#     name: "Pennsylvania",
-#     capital: "Harrisburg"
-# }, {
-#     name: "Rhode Island",
-#     capital: "Providence"
-# }, {
-#     name: "South Carolina",
-#     capital: "Columbia"
-# }, {
-#     name: "South Dakota",
-#     capital: "Pierre"
-# }, {
-#     name: "Tennessee",
-#     capital: "Nashville"
-# }, {
-#     name: "Texas",
-#     capital: "Austin"
-# }, {
-#     name: "Utah",
-#     capital: "Salt Lake City"
-# }, {
-#     name: "Vermont",
-#     capital: "Montpelier"
-# }, {
-#     name: "Virginia",
-#     capital: "Richmond"
-# }, {
-#     name: "Washington",
-#     capital: "Olympia"
-# }, {
-#     name: "West Virginia",
-#     capital: "Charleston"
-# }, {
-#     name: "Wisconsin",
-#     capital: "Madison"
-# }, {
-#     name: "Wyoming",
-#     capital: "Cheyenne"
+}, {
+    name: "Arkansas",
+    capital: "Little Rock"
+}, {
+    name: "California",
+    capital: "Sacramento"
+}, {
+    name: "Colorado",
+    capital: "Denver"
+}, {
+    name: "Connecticut",
+    capital: "Hartford"
+}, {
+    name: "Delaware",
+    capital: "Dover"
+}, {
+    name: "Florida",
+    capital: "Tallahassee"
+}, {
+    name: "Georgia",
+    capital: "Atlanta"
+}, {
+    name: "Hawaii",
+    capital: "Honolulu"
+}, {
+    name: "Idaho",
+    capital: "Boise"
+}, {
+    name: "Illinois",
+    capital: "Springfield"
+}, {
+    name: "Indiana",
+    capital: "Indianapolis"
+}, {
+    name: "Iowa",
+    capital: "Des Moines"
+}, {
+    name: "Kansas",
+    capital: "Topeka"
+}, {
+    name: "Kentucky",
+    capital: "Frankfort"
+}, {
+    name: "Louisiana",
+    capital: "Baton Rouge"
+}, {
+    name: "Maine",
+    capital: "Augusta"
+}, {
+    name: "Maryland",
+    capital: "Annapolis"
+}, {
+    name: "Massachusetts",
+    capital: "Boston"
+}, {
+    name: "Michigan",
+    capital: "Lansing"
+}, {
+    name: "Minnesota",
+    capital: "St. Paul"
+}, {
+    name: "Mississippi",
+    capital: "Jackson"
+}, {
+    name: "Missouri",
+    capital: "Jefferson City"
+}, {
+    name: "Montana",
+    capital: "Helena"
+}, {
+    name: "Nebraska",
+    capital: "Lincoln"
+}, {
+    name: "Nevada",
+    capital: "Carson City"
+}, {
+    name: "New Hampshire",
+    capital: "Concord"
+}, {
+    name: "New Jersey",
+    capital: "Trenton"
+}, {
+    name: "New Mexico",
+    capital: "Santa Fe"
+}, {
+    name: "New York",
+    capital: "Albany"
+}, {
+    name: "North Carolina",
+    capital: "Raleigh"
+}, {
+    name: "North Dakota",
+    capital: "Bismarck"
+}, {
+    name: "Ohio",
+    capital: "Columbus"
+}, {
+    name: "Oklahoma",
+    capital: "Oklahoma City"
+}, {
+    name: "Oregon",
+    capital: "Salem"
+}, {
+    name: "Pennsylvania",
+    capital: "Harrisburg"
+}, {
+    name: "Rhode Island",
+    capital: "Providence"
+}, {
+    name: "South Carolina",
+    capital: "Columbia"
+}, {
+    name: "South Dakota",
+    capital: "Pierre"
+}, {
+    name: "Tennessee",
+    capital: "Nashville"
+}, {
+    name: "Texas",
+    capital: "Austin"
+}, {
+    name: "Utah",
+    capital: "Salt Lake City"
+}, {
+    name: "Vermont",
+    capital: "Montpelier"
+}, {
+    name: "Virginia",
+    capital: "Richmond"
+}, {
+    name: "Washington",
+    capital: "Olympia"
+}, {
+    name: "West Virginia",
+    capital: "Charleston"
+}, {
+    name: "Wisconsin",
+    capital: "Madison"
+}, {
+    name: "Wyoming",
+    capital: "Cheyenne"
  }]
 
+game_over = false
 
-
+loop do #everything below runs in this loop
 states.shuffle!
+break if game_over
+
 correct = []
 incorrect = []
 
-states.each do |state|
+states.each do |state| #.each another loop for the users input answers
  puts "What is the state capital of #{state[:name]}"
- my_guess = gets.chomp
+ my_guess = gets.chomp #this gets input
  puts "you guessed " + my_guess
-  if  my_guess == state[:capital]
-    puts "YAY!!"
-    correct << state
+  if  my_guess == state[:capital] #if guess is the state capital
+    puts "Correct!!"
+    correct << state #moves to the correct array
   else
     puts "Sorryyy thats wrong"
     incorrect << state
   end
 end
 
+puts "" #for a line break
 puts "correct: #{correct.length}"
 puts "incorrect: #{incorrect.length}"
+puts "" #for a line break
+
+puts "Do you wanna play again(Y or N)?"
+answer = gets.chomp
+answer.upcase!
+game_over=!(answer == "Y")
+end
