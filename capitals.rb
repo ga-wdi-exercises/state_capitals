c92e9dea216e8de37b51134c15df8b660ebcc0e7
Erica Norby <ericanorby@gmail.com>
2017-02-07 12:08:32 -0500

    initial commit

--- capitals.rb
+++ capitals.rb
@@ -20,134 +20,165 @@ states =[
     capital: "Denver"
 }, {
     name: "Connecticut",
-    capital: "Hartford"
-}, {
-    name: "Delaware",
-    capital: "Dover"
-}, {
-    name: "Florida",
-    capital: "Tallahassee"
-}, {
-    name: "Georgia",
-    capital: "Atlanta"
-}, {
-    name: "Hawaii",
-    capital: "Honolulu"
-}, {
-    name: "Idaho",
-    capital: "Boise"
-}, {
-    name: "Illinois",
-    capital: "Springfield"
-}, {
-    name: "Indiana",
-    capital: "Indianapolis"
-}, {
-    name: "Iowa",
-    capital: "Des Moines"
-}, {
-    name: "Kansas",
-    capital: "Topeka"
-}, {
-    name: "Kentucky",
-    capital: "Frankfort"
-}, {
-    name: "Louisiana",
-    capital: "Baton Rouge"
-}, {
-    name: "Maine",
-    capital: "Augusta"
-}, {
-    name: "Maryland",
-    capital: "Annapolis"
-}, {
-    name: "Massachusetts",
-    capital: "Boston"
-}, {
-    name: "Michigan",
-    capital: "Lansing"
-}, {
-    name: "Minnesota",
-    capital: "St. Paul"
-}, {
-    name: "Mississippi",
-    capital: "Jackson"
-}, {
-    name: "Missouri",
-    capital: "Jefferson City"
-}, {
-    name: "Montana",
-    capital: "Helena"
-}, {
-    name: "Nebraska",
-    capital: "Lincoln"
-}, {
-    name: "Nevada",
-    capital: "Carson City"
-}, {
-    name: "New Hampshire",
-    capital: "Concord"
-}, {
-    name: "New Jersey",
-    capital: "Trenton"
-}, {
-    name: "New Mexico",
-    capital: "Santa Fe"
-}, {
-    name: "New York",
-    capital: "Albany"
-}, {
-    name: "North Carolina",
-    capital: "Raleigh"
-}, {
-    name: "North Dakota",
-    capital: "Bismarck"
-}, {
-    name: "Ohio",
-    capital: "Columbus"
-}, {
-    name: "Oklahoma",
-    capital: "Oklahoma City"
-}, {
-    name: "Oregon",
-    capital: "Salem"
-}, {
-    name: "Pennsylvania",
-    capital: "Harrisburg"
-}, {
-    name: "Rhode Island",
-    capital: "Providence"
-}, {
-    name: "South Carolina",
-    capital: "Columbia"
-}, {
-    name: "South Dakota",
-    capital: "Pierre"
-}, {
-    name: "Tennessee",
-    capital: "Nashville"
-}, {
-    name: "Texas",
-    capital: "Austin"
-}, {
-    name: "Utah",
-    capital: "Salt Lake City"
-}, {
-    name: "Vermont",
-    capital: "Montpelier"
-}, {
-    name: "Virginia",
-    capital: "Richmond"
-}, {
-    name: "Washington",
-    capital: "Olympia"
-}, {
-    name: "West Virginia",
-    capital: "Charleston"
-}, {
-    name: "Wisconsin",
-    capital: "Madison"
-}, {
-    name: "Wyoming",
-    capital: "Cheyenne"
-}]
+    capital: "Hartford"}]
+# }, {
+#     name: "Delaware",
+#     capital: "Dover"
+# }, {
+#     name: "Florida",
+#     capital: "Tallahassee"
+# }, {
+#     name: "Georgia",
+#     capital: "Atlanta"
+# }, {
+#     name: "Hawaii",
+#     capital: "Honolulu"
+# }, {
+#     name: "Idaho",
+#     capital: "Boise"
+# }, {
+#     name: "Illinois",
+#     capital: "Springfield"
+# }, {
+#     name: "Indiana",
+#     capital: "Indianapolis"
+# }, {
+#     name: "Iowa",
+#     capital: "Des Moines"
+# }, {
+#     name: "Kansas",
+#     capital: "Topeka"
+# }, {
+#     name: "Kentucky",
+#     capital: "Frankfort"
+# }, {
+#     name: "Louisiana",
+#     capital: "Baton Rouge"
+# }, {
+#     name: "Maine",
+#     capital: "Augusta"
+# }, {
+#     name: "Maryland",
+#     capital: "Annapolis"
+# }, {
+#     name: "Massachusetts",
+#     capital: "Boston"
+# }, {
+#     name: "Michigan",
+#     capital: "Lansing"
+# }, {
+#     name: "Minnesota",
+#     capital: "St. Paul"
+# }, {
+#     name: "Mississippi",
+#     capital: "Jackson"
+# }, {
+#     name: "Missouri",
+#     capital: "Jefferson City"
+# }, {
+#     name: "Montana",
+#     capital: "Helena"
+# }, {
+#     name: "Nebraska",
+#     capital: "Lincoln"
+# }, {
+#     name: "Nevada",
+#     capital: "Carson City"
+# }, {
+#     name: "New Hampshire",
+#     capital: "Concord"
+# }, {
+#     name: "New Jersey",
+#     capital: "Trenton"
+# }, {
+#     name: "New Mexico",
+#     capital: "Santa Fe"
+# }, {
+#     name: "New York",
+#     capital: "Albany"
+# }, {
+#     name: "North Carolina",
+#     capital: "Raleigh"
+# }, {
+#     name: "North Dakota",
+#     capital: "Bismarck"
+# }, {
+#     name: "Ohio",
+#     capital: "Columbus"
+# }, {
+#     name: "Oklahoma",
+#     capital: "Oklahoma City"
+# }, {
+#     name: "Oregon",
+#     capital: "Salem"
+# }, {
+#     name: "Pennsylvania",
+#     capital: "Harrisburg"
+# }, {
+#     name: "Rhode Island",
+#     capital: "Providence"
+# }, {
+#     name: "South Carolina",
+#     capital: "Columbia"
+# }, {
+#     name: "South Dakota",
+#     capital: "Pierre"
+# }, {
+#     name: "Tennessee",
+#     capital: "Nashville"
+# }, {
+#     name: "Texas",
+#     capital: "Austin"
+# }, {
+#     name: "Utah",
+#     capital: "Salt Lake City"
+# }, {
+#     name: "Vermont",
+#     capital: "Montpelier"
+# }, {
+#     name: "Virginia",
+#     capital: "Richmond"
+# }, {
+#     name: "Washington",
+#     capital: "Olympia"
+# }, {
+#     name: "West Virginia",
+#     capital: "Charleston"
+# }, {
+#     name: "Wisconsin",
+#     capital: "Madison"
+# }, {
+#     name: "Wyoming",
+#     capital: "Cheyenne"
+# }]
+
+puts "\nLet's test your knowledge of state capitals!\n~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~\n\n"
+
+states.shuffle.each do |state|
+  state[:correct] = 0
+  state[:wrong] = 0
+end
+
+play_number = 0
+
+play_game = "Y"
+
+while play_game == "Y"
+  play_number += 1
+  total_score = 0
+  states.sort_by{|hash| hash[:correct]}.each do |state|
+    puts state[:name]
+    user_answer = gets.chomp
+    if user_answer == state[:capital]
+      state[:correct] += 1
+      total_score += 1
+      puts "Correct! You have guessed correctly #{state[:correct]} out of #{play_number} time(s).\n\n"
+    else
+      state[:wrong] += 1
+      puts "Wrong! You have guessed correctly #{state[:correct]} out of #{play_number} time(s).\n\n"
+    end
+  end
+  puts "Your total score is #{total_score}/#{states.length}"
+  puts "Would you like to play again? (Y/N)"
+  play_game = gets.chomp
+end
