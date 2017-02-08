a9c8ea36f98d72dc3f84f048332ae07b29fc0803
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-02-08 16:37:41 -0500

    printing, would yo like to play again

--- capitals.rb
+++ capitals.rb
@@ -10,164 +10,164 @@ states =[
     capital: "Juneau"
 }, {
     name: "Arizona",
-    capital: "Phoenix"
+    capital: "Phoenix",
+}, {
+    name: "Arkansas",
+    capital: "Little Rock"
+}, {
+    name: "California",
+    capital: "Sacramento"
+}, {
+    name: "Colorado",
+    capital: "Denver"
+}, {
+    name: "Connecticut",
+    capital: "Hartford"
+}, {
+    name: "Delaware",
+    capital: "Dover"
+}, {
+    name: "Florida",
+    capital: "Tallahassee"
+}, {
+    name: "Georgia",
+    capital: "Atlanta"
+}, {
+    name: "Hawaii",
+    capital: "Honolulu"
+}, {
+    name: "Idaho",
+    capital: "Boise"
+}, {
+    name: "Illinois",
+    capital: "Springfield"
+}, {
+    name: "Indiana",
+    capital: "Indianapolis"
+}, {
+    name: "Iowa",
+    capital: "Des Moines"
+}, {
+    name: "Kansas",
+    capital: "Topeka"
+}, {
+    name: "Kentucky",
+    capital: "Frankfort"
+}, {
+    name: "Louisiana",
+    capital: "Baton Rouge"
+}, {
+    name: "Maine",
+    capital: "Augusta"
+}, {
+    name: "Maryland",
+    capital: "Annapolis"
+}, {
+    name: "Massachusetts",
+    capital: "Boston"
+}, {
+    name: "Michigan",
+    capital: "Lansing"
+}, {
+    name: "Minnesota",
+    capital: "St. Paul"
+}, {
+    name: "Mississippi",
+    capital: "Jackson"
+}, {
+    name: "Missouri",
+    capital: "Jefferson City"
+}, {
+    name: "Montana",
+    capital: "Helena"
+}, {
+    name: "Nebraska",
+    capital: "Lincoln"
+}, {
+    name: "Nevada",
+    capital: "Carson City"
+}, {
+    name: "New Hampshire",
+    capital: "Concord"
+}, {
+    name: "New Jersey",
+    capital: "Trenton"
+}, {
+    name: "New Mexico",
+    capital: "Santa Fe"
+}, {
+    name: "New York",
+    capital: "Albany"
+}, {
+    name: "North Carolina",
+    capital: "Raleigh"
+}, {
+    name: "North Dakota",
+    capital: "Bismarck"
+}, {
+    name: "Ohio",
+    capital: "Columbus"
+}, {
+    name: "Oklahoma",
+    capital: "Oklahoma City"
+}, {
+    name: "Oregon",
+    capital: "Salem"
+}, {
+    name: "Pennsylvania",
+    capital: "Harrisburg"
+}, {
+    name: "Rhode Island",
+    capital: "Providence"
+}, {
+    name: "South Carolina",
+    capital: "Columbia"
+}, {
+    name: "South Dakota",
+    capital: "Pierre"
+}, {
+    name: "Tennessee",
+    capital: "Nashville"
+}, {
+    name: "Texas",
+    capital: "Austin"
+}, {
+    name: "Utah",
+    capital: "Salt Lake City"
+}, {
+    name: "Vermont",
+    capital: "Montpelier"
+}, {
+    name: "Virginia",
+    capital: "Richmond"
+}, {
+    name: "Washington",
+    capital: "Olympia"
+}, {
+    name: "West Virginia",
+    capital: "Charleston"
+}, {
+    name: "Wisconsin",
+    capital: "Madison"
+}, {
+    name: "Wyoming",
+    capital: "Cheyenne"
 }]
-# }, {
-#     name: "Arkansas",
-#     capital: "Little Rock"
-# }, {
-#     name: "California",
-#     capital: "Sacramento"
-# }, {
-#     name: "Colorado",
-#     capital: "Denver"
-# }, {
-#     name: "Connecticut",
-#     capital: "Hartford"
-# }, {
-#     name: "Delaware",
-#     capital: "Dover"
-# }, {
-#     name: "Florida",
-#     capital: "Tallahassee"
-# }, {
-#     name: "Georgia",
-#     capital: "Atlanta"
-# }, {
-#     name: "Hawaii",
-#     capital: "Honolulu"
-# }, {
-#     name: "Idaho",
-#     capital: "Boise"
-# }, {
-#     name: "Illinois",
-#     capital: "Springfield"
-# }, {
-#     name: "Indiana",
-#     capital: "Indianapolis"
-# }, {
-#     name: "Iowa",
-#     capital: "Des Moines"
-# }, {
-#     name: "Kansas",
-#     capital: "Topeka"
-# }, {
-#     name: "Kentucky",
-#     capital: "Frankfort"
-# }, {
-#     name: "Louisiana",
-#     capital: "Baton Rouge"
-# }, {
-#     name: "Maine",
-#     capital: "Augusta"
-# }, {
-#     name: "Maryland",
-#     capital: "Annapolis"
-# }, {
-#     name: "Massachusetts",
-#     capital: "Boston"
-# }, {
-#     name: "Michigan",
-#     capital: "Lansing"
-# }, {
-#     name: "Minnesota",
-#     capital: "St. Paul"
-# }, {
-#     name: "Mississippi",
-#     capital: "Jackson"
-# }, {
-#     name: "Missouri",
-#     capital: "Jefferson City"
-# }, {
-#     name: "Montana",
-#     capital: "Helena"
-# }, {
-#     name: "Nebraska",
-#     capital: "Lincoln"
-# }, {
-#     name: "Nevada",
-#     capital: "Carson City"
-# }, {
-#     name: "New Hampshire",
-#     capital: "Concord"
-# }, {
-#     name: "New Jersey",
-#     capital: "Trenton"
-# }, {
-#     name: "New Mexico",
-#     capital: "Santa Fe"
-# }, {
-#     name: "New York",
-#     capital: "Albany"
-# }, {
-#     name: "North Carolina",
-#     capital: "Raleigh"
-# }, {
-#     name: "North Dakota",
-#     capital: "Bismarck"
-# }, {
-#     name: "Ohio",
-#     capital: "Columbus"
-# }, {
-#     name: "Oklahoma",
-#     capital: "Oklahoma City"
-# }, {
-#     name: "Oregon",
-#     capital: "Salem"
-# }, {
-#     name: "Pennsylvania",
-#     capital: "Harrisburg"
-# }, {
-#     name: "Rhode Island",
-#     capital: "Providence"
-# }, {
-#     name: "South Carolina",
-#     capital: "Columbia"
-# }, {
-#     name: "South Dakota",
-#     capital: "Pierre"
-# }, {
-#     name: "Tennessee",
-#     capital: "Nashville"
-# }, {
-#     name: "Texas",
-#     capital: "Austin"
-# }, {
-#     name: "Utah",
-#     capital: "Salt Lake City"
-# }, {
-#     name: "Vermont",
-#     capital: "Montpelier"
-# }, {
-#     name: "Virginia",
-#     capital: "Richmond"
-# }, {
-#     name: "Washington",
-#     capital: "Olympia"
-# }, {
-#     name: "West Virginia",
-#     capital: "Charleston"
-# }, {
-#     name: "Wisconsin",
-#     capital: "Madison"
-# }, {
-#     name: "Wyoming",
-#     capital: "Cheyenne"
-# }]
 
-# PSEUDO CODE, MAKE A PULL REQUEST RIGHT AWAY!
-# prompt the user to identify the capital associated with a given state
-# tally on the number of correct and incorrect answers for each state
-# after getting through all 50 states one time, ask the user if they would like to play again
-puts "Hey, here's your chance to test your wits with knowing the capitals of every state in America."
+# write a shuffle the states
+  states = states.shuffle
 
-# write a loop to shuffle the states
+# create a welcome message to introduce the player to the game
+  puts "Hey, here's your chance to test your wits with knowing the capitals of every state in America."
+
 # initialize new keys in the hashes that store the number of times a user gets a capital correct + wrong
+# tally on the number of correct and incorrect answers for each state
   states.each do |state|
     state[:num_correct_answer] = 0
     state [:num_wrong_answer] = 0
   end
 
+# prompt the user to identify the capital associated with a given state
 # make the player play through all 50 states by prompting the player to name the capital of each state
 while true
   states.each do |state|
@@ -175,13 +175,20 @@ while true
 
     answer = gets.chomp
 
-if answer == state[:capital]
+if answer.upcase == state[:capital].upcase
   puts "Correct Answer!"
   state[:num_correct_answer] +=1
 else
   puts "Your absolutely wrong, the capital is #{state[:capital]}"
   state[:num_wrong_answer] +=1
 end
+
+# after getting through all 50 states one time, ask the user if they would like to play again
+
+puts "That was a great first try, would you like to play again (Y/N)?"
+  answer = gets.chomp
+  break if answer.upcase == "N"
+
 
 
 
@@ -194,8 +201,7 @@ end
 
 
 # shuffle the states to make it challenging
-# create a welcome message to introduce the player to the game
-puts "Welcome to the remember all of the state capitals game! Where learning is fun."
+
 
 
 
