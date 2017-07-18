5d2733498de97950857b1fd6f1ea93cfc1e26010
Sanjay Persaud <sanjaywallah@gmail.com>
2017-07-18 17:08:26 -0400

    added final logic

--- capitals.rb
+++ capitals.rb
@@ -163,22 +163,38 @@ puts "Welcome to the State Capitals Game!"
 puts "Please enter your name"
 user[:name] = gets.chomp
 
+#Method to Run Game
+def game(user, states)
+  # states.length.times do |index|
+  2.times do |index|
+      correct = user[:score_correct]
+      puts correct
+      puts "what is the capital of #{states[index][:name]} \ntotal Correct: #{correct} out of #{index}"
+      input = gets.chomp
+
+      if(input == states[index][:capital])
+        puts "That's Correct!  The Capital of #{states[index][:name]} is #{states[index][:capital]}"
+        user[:score_correct] +=1
+      else
+        puts "Sorry, that's incorrect! The Capital of #{states[index][:name]} is #{states[index][:capital]}"
+        user[:score_incorrect] +=1
+      end
+    end
+end
+
 states.shuffle!
+game(user, states)
 
-# states.length.times do |index|
-2.times do |index|
-  correct = user[:score_correct]
-  puts correct
-  puts "what is the capital of #{states[index][:name]}"
-  input = gets.chomp
+puts "do yo wish to play again y/n"
+input2 = gets.chomp
 
-  if input == states[index][:capital]
-    puts "That's Correct"
-    user[:score_correct] +=1
-  else
-    puts "That's Incorrect"
-    user[:score_incorrect] +=1
-  end
-en
-puts user[:score_correct]
-puts user[:score_incorrect]
+while input2 == 'y'
+  states.shuffle!
+  user[:score_correct]=0
+  user[:score_incorrect]=0
+  game(user,states)
+  puts "do you wish to play again y/n"
+  input2 = gets.chomp
+end
+
+puts "goodbye"
