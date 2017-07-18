bb1b34078b115a9d3cc6fd17f93d385adbc0cc03
Charles Cox <cox.charles.w@gmail.com>
2017-07-18 14:12:34 -0400

    store number of correct or wrong answers and report the number of correct answers after each guess

--- capitals.rb
+++ capitals.rb
@@ -13,10 +13,12 @@ states =[
 
 
 #welcome message*
-#Randomize the states
+#Randomize the states*
 #store correct and wrong answers (number of)
-#loop through all the states
-#take in a guess for each state
+#loop through all the states*
+#take in a guess for each state*
+#tell how many times it has been guessed correct out of total
+#ask if the user would like to play again
 
 puts "Welcome, ready to test your knowledge of state capitals?"
 
@@ -26,9 +28,19 @@ states.length.times do |index|
   state_to_guess = states[index][:name]
   puts "What is the capital of #{state_to_guess}?"
   guess = gets.chomp
+  states[index][:correct] = 0
+  states[index][:wrong] = 0
   if guess == states[index][:capital]
-    puts 'Correct'
+    states[index][:correct] =+ 1
+    right = states[index][:correct]
+    total = states[index][:correct] + states[index][:wrong]
+    puts "Correct. You have gotten this state correct #{right} out of #{total} attempts"
   elsif
     puts 'Wrong'
+    states[index][:wrong] =+ 1
+    right = states[index][:correct]
+    total = states[index][:correct] + states[index][:wrong]
+    puts "Correct. You have gotten this state correct #{right} out of #{total} attempts"
   end
 end
+  
