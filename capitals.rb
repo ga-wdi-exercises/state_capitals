1824563a880981adacfbf19ec72b771bd7ae97ef
Charles Cox <cox.charles.w@gmail.com>
2017-07-18 15:56:57 -0400

    add bonus features

--- capitals.rb
+++ capitals.rb
@@ -18,50 +18,59 @@ states =[
 #loop through all the states*
 #take in a guess for each state*
 #tell how many times it has been guessed correct out of total*
-#ask if the user would like to play again
-
-puts "Welcome, ready to test your knowledge of state capitals?"
-
-states =[
-{
-    name: "Alabama",
-    capital: "Montgomery"
-}, {
-    name: "Alaska",
-    capital: "Juneau"
-}, {
-    name: "Arizona",
-    capital: "Phoenix"
-}]
+#ask if the user would like to play again*
 
 puts "Welcome, ready to test your knowledge of state capitals?"
 
 play = true
 
 states = states.shuffle
+total_correct = 0
+states.each do |state|
+  state[:correct] = 0
+  state[:wrong] = 0
+  state[:total] = 0
+end
 
 while play == true do
-  states.length.times do |index|
-    state_to_guess = states[index][:name]
-    puts "What is the capital of #{state_to_guess}?"
+
+  states.each do |state|
+
+    state_to_guess = state[:name]
+    puts "What is the capital of #{state_to_guess}? (type 'hint' for a hint)."
     guess = gets.chomp
-    states[index][:correct] = 0
-    states[index][:wrong] = 0
-    if guess == states[index][:capital]
-      states[index][:correct] =+ 1
-      right = states[index][:correct]
-      total = states[index][:correct] + states[index][:wrong]
-      puts "Correct. You have gotten this state correct #{right} out of #{total} attempts"
-    elsif
-      states[index][:wrong] =+ 1
-      right = states[index][:correct]
-      total = states[index][:correct] + states[index][:wrong]
-      puts "Correct. You have gotten this state correct #{right} out of #{total} attempts"
+
+    if guess == 'hint'
+      capital_start = state[:capital][0,3]
+      puts "The capital starts with #{capital_start}."
+      puts "What is the capital of #{state_to_guess}?"
+      guess = gets.chomp
     end
+
+    if guess == state[:capital]
+      state[:correct] += 1
+      state[:total] += 1
+      total_correct += 1
+      puts "Correct. You have gotten this state correct #{state[:correct]} out of #{state[:total]} attempts"
+      puts "You have gotten #{total_correct} states right this round."
+      puts "----------------"
+
+    else
+      state[:wrong] += 1
+      state[:total] += 1
+      puts "Wrong. You have gotten this state correct #{state[:correct]} out of #{state[:total]} attempts"
+      puts "You have gotten #{total_correct} states right this round."
+      puts "----------------"
+    end
+
   end
-  puts "Do you want to play again?"
+
+  puts "You got #{total_correct} correct this round. Do you want to play again?"
   again = gets.chomp
-  if again != 'yes' || 'Yes' || 'y' || 'Y'
-    play == false
+  if (again != 'yes') && (again != 'Yes') && (again != 'y') && (again != 'Y')
+    play = false
+  else
+    total_correct = 0
+    states = states.sort_by{ |item| item[:correct] }
   end
 end
