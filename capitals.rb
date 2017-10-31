37514f7a836c22a38b0cc7ac27977f6da8e0a5b8
Michael Ortiz <ortiz_michael@bah.com>
2017-10-31 16:39:01 -0400

    created randomized order functionality

--- capitals.rb
+++ capitals.rb
@@ -1,158 +1,5 @@
-# an array of state hashes
-#states =[
-#{
-#    name: "Alabama",
-#    capital: "Montgomery"
-#}, {
-#    name: "Alaska",
-#    capital: "Juneau"
-#}, {
-#    name: "Arizona",
-#    capital: "Phoenix"
-#}, {
-#    name: "Arkansas",
-#    capital: "Little Rock"
-#}, {
-#    name: "California",
-#    capital: "Sacramento"
-#}, {
-#    name: "Colorado",
-#    capital: "Denver"
-#}, {
-#    name: "Connecticut",
-#    capital: "Hartford"
-#}, {
-#    name: "Delaware",
-#    capital: "Dover"
-#}, {
-#    name: "Florida",
-#    capital: "Tallahassee"
-#}, {
-#    name: "Georgia",
-#    capital: "Atlanta"
-#}, {
-#    name: "Hawaii",
-#    capital: "Honolulu"
-#}, {
-#    name: "Idaho",
-#    capital: "Boise"
-#}, {
-#    name: "Illinois",
-#    capital: "Springfield"
-#}, {
-#    name: "Indiana",
-#    capital: "Indianapolis"
-#}, {
-#    name: "Iowa",
-#    capital: "Des Moines"
-#}, {
-#    name: "Kansas",
-#    capital: "Topeka"
-#}, {
-#    name: "Kentucky",
-#    capital: "Frankfort"
-#}, {
-#    name: "Louisiana",
-#    capital: "Baton Rouge"
-#}, {
-#    name: "Maine",
-#    capital: "Augusta"
-#}, {
-#    name: "Maryland",
-#    capital: "Annapolis"
-#}, {
-#    name: "Massachusetts",
-#    capital: "Boston"
-#}, {
-#    name: "Michigan",
-#    capital: "Lansing"
-#}, {
-#    name: "Minnesota",
-#    capital: "St. Paul"
-#}, {
-#    name: "Mississippi",
-#    capital: "Jackson"
-#}, {
-#    name: "Missouri",
-#    capital: "Jefferson City"
-#}, {
-#    name: "Montana",
-#    capital: "Helena"
-#}, {
-#    name: "Nebraska",
-#    capital: "Lincoln"
-#}, {
-#    name: "Nevada",
-#    capital: "Carson City"
-#}, {
-#    name: "New Hampshire",
-#    capital: "Concord"
-#}, {
-#    name: "New Jersey",
-#    capital: "Trenton"
-#}, {
-#    name: "New Mexico",
-#    capital: "Santa Fe"
-#}, {
-#    name: "New York",
-#    capital: "Albany"
-#}, {
-#    name: "North Carolina",
-#    capital: "Raleigh"
-#}, {
-#    name: "North Dakota",
-#    capital: "Bismarck"
-#}, {
-#    name: "Ohio",
-#    capital: "Columbus"
-#}, {
-#    name: "Oklahoma",
-#    capital: "Oklahoma City"
-#}, {
-#    name: "Oregon",
-#    capital: "Salem"
-#}, {
-#    name: "Pennsylvania",
-#    capital: "Harrisburg"
-#}, {
-#    name: "Rhode Island",
-#    capital: "Providence"
-#}, {
-#    name: "South Carolina",
-#    capital: "Columbia"
-#}, {
-#    name: "South Dakota",
-#    capital: "Pierre"
-#}, {
-#    name: "Tennessee",
-#    capital: "Nashville"
-#}, {
-#    name: "Texas",
-#    capital: "Austin"
-#}, {
-#    name: "Utah",
-#    capital: "Salt Lake City"
-#}, {
-#    name: "Vermont",
-#    capital: "Montpelier"
-#}, {
-#    name: "Virginia",
-#    capital: "Richmond"
-#}, {
-#    name: "Washington",
-#    capital: "Olympia"
-#}, {
-#    name: "West Virginia",
-#    capital: "Charleston"
-#}, {
-#    name: "Wisconsin",
-#    capital: "Madison"
-#}, {
-#    name: "Wyoming",
-#    capital: "Cheyenne"
-#}]
 
-test_states =[
+states =[
 {
     name: "Alabama",
     capital: "Montgomery"
@@ -166,13 +13,147 @@ test_states =[
     name: "Arkansas",
     capital: "Little Rock"
 }, {
+    name: "California",
+    capital: "Sacramento"
+}, {
+    name: "Colorado",
+    capital: "Denver"
+}, {
+    name: "Connecticut",
+    capital: "Hartford"
+}, {
+    name: "Delaware",
+    capital: "Dover"
+}, {
+    name: "Florida",
+    capital: "Tallahassee"
+}, {
+    name: "Georgia",
+    capital: "Atlanta"
+}, {
+    name: "Hawaii",
+    capital: "Honolulu"
+}, {
+    name: "Idaho",
+    capital: "Boise"
+}, {
+    name: "Illinois",
+    capital: "Springfield"
+}, {
+    name: "Indiana",
+    capital: "Indianapolis"
+}, {
+    name: "Iowa",
+    capital: "Des Moines"
+}, {
+    name: "Kansas",
+    capital: "Topeka"
+}, {
+    name: "Kentucky",
+    capital: "Frankfort"
+}, {
+    name: "Louisiana",
+    capital: "Baton Rouge"
+}, {
+    name: "Maine",
+    capital: "Augusta"
+}, {
+    name: "Maryland",
+    capital: "Annapolis"
+}, {
+    name: "Massachusetts",
+    capital: "Boston"
+}, {
+    name: "Michigan",
+    capital: "Lansing"
+}, {
+    name: "Minnesota",
+    capital: "St. Paul"
+}, {
+    name: "Mississippi",
+    capital: "Jackson"
+}, {
+    name: "Missouri",
+    capital: "Jefferson City"
+}, {
+    name: "Montana",
+    capital: "Helena"
+}, {
+    name: "Nebraska",
+    capital: "Lincoln"
+}, {
+    name: "Nevada",
+    capital: "Carson City"
+}, {
+    name: "New Hampshire",
+    capital: "Concord"
+}, {
+    name: "New Jersey",
+    capital: "Trenton"
+}, {
+    name: "New Mexico",
+    capital: "Santa Fe"
+}, {
+    name: "New York",
+    capital: "Albany"
+}, {
+    name: "North Carolina",
+    capital: "Raleigh"
+}, {
+    name: "North Dakota",
+    capital: "Bismarck"
+}, {
+    name: "Ohio",
+    capital: "Columbus"
+}, {
+    name: "Oklahoma",
+    capital: "Oklahoma City"
+}, {
+    name: "Oregon",
+    capital: "Salem"
+}, {
+    name: "Pennsylvania",
+    capital: "Harrisburg"
+}, {
+    name: "Rhode Island",
+    capital: "Providence"
+}, {
+    name: "South Carolina",
+    capital: "Columbia"
+}, {
+    name: "South Dakota",
+    capital: "Pierre"
+}, {
+    name: "Tennessee",
+    capital: "Nashville"
+}, {
+    name: "Texas",
+    capital: "Austin"
+}, {
+    name: "Utah",
+    capital: "Salt Lake City"
+}, {
+    name: "Vermont",
+    capital: "Montpelier"
+}, {
+    name: "Virginia",
+    capital: "Richmond"
+}, {
+    name: "Washington",
+    capital: "Olympia"
+}, {
+    name: "West Virginia",
+    capital: "Charleston"
+}, {
+    name: "Wisconsin",
+    capital: "Madison"
+}, {
     name: "Wyoming",
     capital: "Cheyenne"
 }]
 
-
-states = test_states.shuffle
-  # creates random order of the states
+states = states.shuffle
+  # creates instance of the states
 puts "Welcome! This is the State Capitals game. Let's see if you paid attention in Social Studies!"
   # the welcome
 states.each do |state|
@@ -182,7 +163,7 @@ end
   # for each state, store number of times a player gets the state right or wrong
 while true
   # until user decides to not play
-  test_states.each do |state|
+  states.each do |state|
     puts "What is the capital of " + state[:name] + "?"
     answer=gets.chomp
       # if the player is correct add 1 to the correct counter
