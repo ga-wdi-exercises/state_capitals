c5843d578610f27506ca80589c5c9c71abbba00c
Stella <sgordonzigel@gmail.com>
2017-10-31 15:58:53 -0400

    working capitals game

--- capitals.rb
+++ capitals.rb
@@ -150,4 +150,39 @@ states =[
 }, {
     name: "Wyoming",
     capital: "Cheyenne"
-}]
+}].shuffle!
+
+#adds number of correct answers, wrong answers, and total guesses to each states' hash
+states.each { |state| state[:num_correct] = 0 }
+states.each { |state| state[:num_wrong] = 0 }
+states.each { |state| state[:num_total_guesses] = 0 }
+
+def play_game(states)
+  states.each do |state|
+    puts "Enter the Capital of #{state[:name]}"
+    answer = gets.chomp.to_s
+    if answer == state[:capital]
+      state[:num_correct] += 1
+      state[:num_total_guesses] += 1
+      puts "You are correct! You have guessed this capital correctly #{state[:num_correct]} out of #{state[:num_total_guesses]} guesses"
+    else
+      state[:num_wrong] += 1
+      state[:num_total_guesses] += 1
+      puts "You are wrong. You have guess this capital correctly #{state[:num_correct]} out of #{state[:num_total_guesses]} guesses"
+    end
+  end
+  puts "Do you want to play again? yes or no"
+  answer = gets.chomp
+  if answer == "yes"
+    #if user wants to play again, they will now be playing from the sorted_states array
+    sorted_states = []
+    sorted_states = states.sort_by{ |state| state[:num_correct]}
+    play_game(sorted_states)
+  end
+end
+
+puts "Welcome to the State Capitals game! Type play to begin."
+answer = gets.chomp
+if answer == "play"
+  play_game(states)
+end
