1b10cf4b70bfbf86551975119560b150c5cc6bbf
SAMUEL HAILE <samishken@gmail.com>
2018-04-12 23:19:32 -0400

    shuffle loop working

--- capitals.rb
+++ capitals.rb
@@ -9,20 +9,35 @@ states =[
 }, {
     name: "Arizona",
     capital: "Phoenix"
-}]
+}, {
+    name: "Arkansas",
+    capital: "Little Rock"
+}, {
+    name: "California",
+    capital: "Sacramento"
+}, {
+    name: "Colorado",
+    capital: "Denver"
+}
+]
 
 
-# puts "hello world"
-
-# puts "Can you name the capital cities?"
-# states = gets.chomp
-
-input = ""
-puts "Hello"
-while input != "hello" do
-  puts states.shuffle
-  input = gets.chomp
+states.each do |state|
+    state[:correct] = 0
+    state[:wrong] = 0
 end
-puts "Good job!"
 
+puts "Welcome! This games tests your knowlege of U.S. capitals. Enter the capital of each state that appears."
 
+states.shuffle!
+    states.map do |state|
+        puts "What is the capital of " + state[:name] + "?"
+        input = gets.chomp
+        if input == state[:capital]
+            state[:correct] += 1 
+            puts "#{input} is correct"
+        else
+            state[:wrong] += 1
+            puts "#{input} is incorrect"
+        end
+    end
